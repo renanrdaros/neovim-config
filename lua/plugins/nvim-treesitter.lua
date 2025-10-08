23a9d1afaf6af1fd4644d4ff578ed319e4f77457
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2025-10-08 14:51:41 -0300

    Add css parser for treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,7 +6,20 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup {
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = { "c", "cpp", "python", "lua", "cmake", "vim", "vimdoc", "query", "markdown", "html", "xml", },
+      ensure_installed = {
+        "c",
+        "cpp",
+        "python",
+        "lua",
+        "cmake",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "html",
+        "xml",
+        "css",
+      },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
