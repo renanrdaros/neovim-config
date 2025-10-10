663dd574748ea3581b6ca12a0618b6bbbe1c10f1
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2025-10-10 20:44:36 -0300

    Add plugin gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,43 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+    -- Navigation
+      map('n', '[h', function()
+        if vim.wo.diff then
+          vim.cmd.normal({'[c', bang = true})
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end, { desc = "Go to previous hunk" })
+
+      map('n', ']h', function()
+        if vim.wo.diff then
+          vim.cmd.normal({']c', bang = true})
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end, { desc = "Go to next hunk" })
+
+      map('n', '[H', function() gitsigns.nav_hunk("first") end, { desc = "Go to first hunk" })
+      map('n', ']H', function() gitsigns.nav_hunk("last") end, { desc = "Go to last hunk" })
+
+    -- Actions
+      map('n', '<leader>Gp', gitsigns.preview_hunk)
+      map('n', '<leader>Gi', gitsigns.preview_hunk_inline)
+      map('n', '<leader>Gb', function() gitsigns.blame_line({ full = true }) end, { desc = "Blame current line" })
+      map('n', '<leader>Gd', gitsigns.diffthis)
+      map('n', '<leader>GD', function() gitsigns.diffthis('~') end)
+      map('n', '<leader>GB', gitsigns.toggle_current_line_blame)
+      map('n', '<leader>GW', gitsigns.toggle_word_diff)
+    end
+  },
+}
