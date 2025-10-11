76f2a62ac9c57acad96ea7ed9991270edba74d6b
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2025-10-11 15:44:31 -0300

    Add descriptions to some gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -31,13 +31,13 @@ return {
       map('n', ']H', function() gitsigns.nav_hunk("last") end, { desc = "Go to last hunk" })
 
     -- Actions
-      map('n', '<leader>Gp', gitsigns.preview_hunk)
-      map('n', '<leader>Gi', gitsigns.preview_hunk_inline)
+      map('n', '<leader>Gp', gitsigns.preview_hunk, { desc = "Preview hunk in a floating window" })
+      map('n', '<leader>Gi', gitsigns.preview_hunk_inline, { desc = "Preview hunk inline in the buffer" })
       map('n', '<leader>Gb', function() gitsigns.blame_line({ full = true }) end, { desc = "Blame current line" })
-      map('n', '<leader>Gd', gitsigns.diffthis)
-      map('n', '<leader>GD', function() gitsigns.diffthis('~') end)
-      map('n', '<leader>GB', gitsigns.toggle_current_line_blame)
-      map('n', '<leader>GW', gitsigns.toggle_word_diff)
+      map('n', '<leader>Gd', gitsigns.diffthis, { desc = "Diff against the index" })
+      map('n', '<leader>GD', function() gitsigns.diffthis('~') end, { desc = "Diff against the last commit" })
+      map('n', '<leader>GB', gitsigns.toggle_current_line_blame, { desc = "Toggle blame annotation at the end of current line" })
+      map('n', '<leader>GW', gitsigns.toggle_word_diff, { desc = "Toggle highlighting of word differences in the buffer" })
     end
   },
 }
