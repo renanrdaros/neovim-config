ca7413733f5e063457f2f9456fd3c2d425237123
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2025-10-12 18:36:44 -0300

    Update render-markdown configs

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -3,8 +3,14 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  -- opts = {},
+  ft = {"md", "markdown"},
   keys = {
     { "<Leader>mr", function() require("render-markdown").toggle() end, desc = "Toggle render-markdown" },
   },
+  config = function()
+    local render_md = require("render-markdown")
+    render_md.setup()
+    render_md.disable()
+  end,
 }
