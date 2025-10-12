7b525685fa2a4281fd04d8fb5023dfd349780d2e
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2025-10-12 10:08:19 -0300

    Add tab navigation keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
+-- navigation
+vim.keymap.set("n", "[<Tab>", ":tabprevious<CR>", { desc = "Go to the previous tab page" })
+vim.keymap.set("n", "]<Tab>", ":tabnext<CR>", { desc = "Go to the next tab page" })
+
 -- toggles
 local function toggle(opt) vim.opt[opt] = not vim.opt[opt]:get() end
 vim.keymap.set("n", "<Leader>cc", function() toggle("cursorcolumn") end, { desc = "Toggle cursor column" })
