ddb276eda8dd41a9ec51e1b0f8d823270c884e78
Renan Rocha Darós <14208755+renanrdaros@users.noreply.github.com>
2025-10-22 07:38:39 -0300

    Change live-preview keymaps

--- lua/plugins/live-preview.lua
+++ lua/plugins/live-preview.lua
@@ -2,8 +2,8 @@ return {
   'brianhuster/live-preview.nvim',
   dependencies = { 'ibhagwan/fzf-lua' },
   keys = {
-    { "<Leader>ls", ":LivePreview start<CR>", desc = "Start live preview of current file" },
-    { "<Leader>lc", ":LivePreview close<CR>", desc = "Close live preview server" },
-    { "<Leader>lp", ":LivePreview pick<CR>", desc = "Pick a file to preview" },
+    { "<Leader>ps", ":LivePreview start<CR>", desc = "Start live preview of current file" },
+    { "<Leader>pc", ":LivePreview close<CR>", desc = "Close live preview server" },
+    { "<Leader>pp", ":LivePreview pick<CR>", desc = "Pick a file to preview" },
   },
 }
